dbb1299c30e4ffb27ff6d368d5028860856303c3
csteinmetz1 <csteinmetz1@gmail.com>
2019-02-21 23:23:53 -0500

    typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'pyreqs',
     version = '0.1.0',
     packages = ['pyreqs'],
-    install_requires=['stdlib_list']
+    install_requires=['stdlib_list'],
     entry_points = {
         'console_scripts': [
             'pyreqs = pyreqs.__main__:main'
